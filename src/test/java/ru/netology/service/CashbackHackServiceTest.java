64ebbb07800190e0ed3175716ccbe62bcccdb31f
Денис Сергеев <D-D1983@yandex.ru>
2022-05-31 11:42:20 +0400

    tests added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,7 +1,43 @@
 package ru.netology.service;
 
-import static org.testng.Assert.*;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class CashbackHackServiceTest {
 
+
+    @Test
+    public void shouldGetRemainIfZero() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(0);
+        Assert.assertEquals(actual, 1000);
+    }
+
+    @Test
+    public void shouldGetRemainIfLessOneBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(950);
+        Assert.assertEquals(actual, 50);
+    }
+
+    @Test
+    public void shouldGetRemainIfMoreOneBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1400);
+        Assert.assertEquals(actual, 600);
+    }
+
+    @Test
+    public void shouldGetRemainIfBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(2000);
+        Assert.assertEquals(actual, 0);
+    }
+
+    @Test
+    public void shouldGetRemainIfHugeAmount() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(15500);
+        Assert.assertEquals(actual, 500);
+    }
 }
